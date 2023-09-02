592a2193669f52ee27961695dc106092359edc83
kent-3 <100624004+kent-3@users.noreply.github.com>
2023-09-01 21:51:23 -0400

    rust only, catppuccin

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,7 +2,7 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  -- { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.pack.python" },
   -- { import = "astrocommunity.pack.bash" },
   -- { import = "astrocommunity.pack.docker" },
