fccc3449f5cf38627e2539f8b53df5c639b36e19
kent-3 <100624004+kent-3@users.noreply.github.com>
2023-11-22 12:37:41 -0500

    change defaults

--- lsp/config/rust_analyzer.lua
+++ lsp/config/rust_analyzer.lua
@@ -2,7 +2,8 @@ return {
   settings = {
     ["rust-analyzer"] = {
       checkOnSave = true,
-      check = { command = "clippy" },
+      -- check = { command = "clippy" },
+      -- check = { command = "check" },
       procMacro = { ignored = { leptos_macro = { "server", "component" } } },
     },
   },
