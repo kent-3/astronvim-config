020b529548e541788095b5ebb6613ebc00ef45d3
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-03-22 13:25:43 -0400

    add highlight overrides

--- plugins/kanagawa.lua
+++ plugins/kanagawa.lua
@@ -22,7 +22,32 @@ return {
           theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
         },
         overrides = function(colors) -- add/modify highlights
-          return {}
+          local theme = colors.theme
+          return {
+            Tag = { link = "Special" },
+
+            -- Markup
+            -- ["@markup"] = { fg = C.text }, -- For strings considerated text in a markup language.
+            -- ["@markup.strong"] = { fg = C.maroon, style = { "bold" } }, -- bold
+            -- ["@markup.italic"] = { fg = C.maroon, style = { "italic" } }, -- italic
+            -- ["@markup.strikethrough"] = { fg = C.text, style = { "strikethrough" } }, -- strikethrough text
+            -- ["@markup.underline"] = { link = "Underlined" }, -- underlined text
+            --
+            -- ["@markup.heading"] = { fg = C.blue, style = { "bold" } }, -- titles like: # Example
+            --
+            -- ["@markup.math"] = { fg = C.blue }, -- math environments (e.g. `$ ... $` in LaTeX)
+            -- ["@markup.environment"] = { fg = C.pink }, -- text environments of markup languages
+            -- ["@markup.environment.name"] = { fg = C.blue }, -- text indicating the type of an environment
+
+            ["@markup.link"] = { link = "Tag" }, -- text references, footnotes, citations, etc.
+            ["@markup.link.url"] = { fg = colors.palette.sakuraPink, italic = true }, -- urls, links and emails
+
+            ["@markup.raw"] = { fg = theme.syn.special1 }, -- used for inline code in markdown and for doc in python (""")
+
+            ["@markup.list"] = { link = "Special" },
+            -- ["@markup.list.checked"] = { fg = theme.syn.string }, -- todo notes
+            -- ["@markup.list.unchecked"] = { fg = theme.syn.comment }, -- todo notes
+          }
         end,
         theme = "wave", -- Load "wave" theme when 'background' option is not set
         background = { -- map the value of 'background' option to a theme
